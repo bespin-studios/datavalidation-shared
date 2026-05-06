84f8a44ac07e9dbd5ec489ad2cc390d6c9cc9f7a
Jonas Bönhoff <28276430+boenhoff@users.noreply.github.com>
2026-05-06 14:44:40 +0200

    feat: add additional supported countries

--- src/Country.php
+++ src/Country.php
@@ -4,6 +4,19 @@ namespace Bespin\DataValidation;
 
 enum Country
 {
+    case Austria;
+    case Belgium;
+    case Croatia;
+    case Denmark;
+    case Finland;
     case France;
     case Germany;
+    case Italy;
+    case Luxembourg;
+    case Netherlands;
+    case Poland;
+    case Portugal;
+    case Slovenia;
+    case Spain;
+    case Sweden;
 }
