beb3de23deeecab8211424f9f565b68b73299019
Lars Hennig <lars@bespin-studios.com>
2026-05-06 21:57:27 +0200

    feat: add enum case for other european countries

--- src/Country.php
+++ src/Country.php
@@ -6,17 +6,30 @@ enum Country
 {
     case Austria;
     case Belgium;
+    case Bulgaria;
     case Croatia;
+    case Cyprus;
+    case CzechRepublic;
     case Denmark;
+    case Estonia;
     case Finland;
     case France;
     case Germany;
+    case Greece;
+    case Hungary;
+    case Ireland;
     case Italy;
+    case Latvia;
+    case Lithuania;
     case Luxembourg;
+    case Malta;
     case Netherlands;
     case Poland;
     case Portugal;
+    case Romania;
+    case Slovakia;
     case Slovenia;
     case Spain;
     case Sweden;
+    case UnitedKingdom;
 }
